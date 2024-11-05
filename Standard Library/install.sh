f9eb6fd4c2206c307eae0cc62b11792cde885b12
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-11-05 08:01:46 -0600

    Update install.sh

--- Standard Library/install.sh
+++ Standard Library/install.sh
@@ -1,7 +1,18 @@
+cd ~/Downloads
+
+get(){
+  wget https://static.rust-lang.org/dist/2024-10-17/rust-src-1.82.0.tar.gz
+}
+
+extract(){
+  tar xfz rust-src-1.82.0.tar.gz
+}
+
+install(){
+  sudo cp -r rust-src-1.82.0/rust-src/lib/rustlib/src /usr/lib/rustlib/
+}
+
+# Old:
 # sudo ./install.sh --list-components
-
 # Install Rust Standard Library to /lib/rustlib
-sudo ./install.sh --components=rust-std-x86_64-unknown-linux-gnu --libdir=/lib
-
-# Solution: https://stackoverflow.com/questions/41464063/how-to-provide-standard-library-sources-for-intellij-ideas-rust-project/49453070#49453070
-sudo ln -s /usr/lib/rustlib/src /usr/lib/rustlib/x86_64-unknown-linux-gnu/
+#sudo ./install.sh --components=rust-std-x86_64-unknown-linux-gnu --libdir=/lib
