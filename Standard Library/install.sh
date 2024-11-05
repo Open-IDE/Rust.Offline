48ecf12ac4d3919a24823db5694d95d63e3fc3f1
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-11-05 07:30:08 -0600

    Update install.sh

--- Standard Library/install.sh
+++ Standard Library/install.sh
@@ -4,5 +4,4 @@
 sudo ./install.sh --components=rust-std-x86_64-unknown-linux-gnu --libdir=/lib
 
 # Solution: https://stackoverflow.com/questions/41464063/how-to-provide-standard-library-sources-for-intellij-ideas-rust-project/49453070#49453070
-# Link:
 sudo ln -s /usr/lib/rustlib/src /usr/lib/rustlib/x86_64-unknown-linux-gnu/
