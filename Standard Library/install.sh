a9fec5d0e35d25e9f6fd5144a4b313ceabe3bc1b
TwinFlame University <61005029+Twin-Flame@users.noreply.github.com>
2024-11-05 09:39:48 -0600

    Update install.sh

--- Standard Library/install.sh
+++ Standard Library/install.sh
@@ -1,19 +1,22 @@
-cd ~/Downloads
+libdir=/usr/lib
+tmpdir=~/Downloads
 
 get(){
+  cd $tmpdir
   wget https://static.rust-lang.org/dist/2024-10-17/rust-src-1.82.0.tar.gz
 }
 
 extract(){
+  cd $tmpdir
   tar xfz rust-src-1.82.0.tar.gz
 }
 
 install(){
-  sudo cp -r rust-src-1.82.0/rust-src/lib/rustlib/src /usr/lib/rustlib/
+  sudo cp -r $tmpdir/rust-src-1.82.0/rust-src/lib/rustlib/src /usr/lib/rustlib/
 }
 
 install_standard_library(){
   # sudo ./install.sh --list-components
   # Install Rust Standard Library to /lib/rustlib
-  sudo ./install.sh --components=rust-std-x86_64-unknown-linux-gnu --libdir=/lib
+  sudo ./install.sh --components=rust-std-x86_64-unknown-linux-gnu --libdir=$libdir
 }
